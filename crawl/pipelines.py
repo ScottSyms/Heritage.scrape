4ec3435d5e0a0162ed1a4a39c7b6a6fa2ea29aba
Scott Syms <scottsyms@SpottedFish.local>
2022-12-29 10:13:03 -0400

    First working spider with playwright

---
+++ crawl/pipelines.py
@@ -0,0 +1,75 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from sqlalchemy import create_engine, Column, Table, ForeignKey, MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Integer, String, Date, DateTime, Float, Boolean, Text)
+from scrapy.utils.project import get_project_settings
+
+
+# class CrawlPipeline:
+#     def process_item(self, item, spider):
+#         return item
+
+Base=declarative_base()
+
+def db_connect():
+    """
+    Performs database connection using database settings from settings.py.
+    Returns sqlalchemy engine instance
+    """
+    return create_engine(get_project_settings().get("CONNECTION_STRING"))
+
+def create_table(engine):
+    Base.metadata.create_all(engine)
+
+
+class Source(Base):
+    __tablename__ = "source"
+    id = Column(Integer, primary_key=True)
+    status=Column(String(3))
+    pairid=Column(Integer)
+    url=Column(String(255))
+    language=Column(String(2))
+    text=Column(Text)
+    alturl=Column(String(255))
+
+class SaveDBPipeline(object):
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker
+        Creates tables
+        """
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    def process_item(self, item, spider):
+        session=self.Session()
+        page=Source()
+        page.status=item['status']
+        page.pairid=item['pairid']
+        page.url=item['url']
+        page.alturl=item['altURL']
+        page.language=item['language']
+        page.text=item['text']
+
+        try:
+            session.add(page)
+            session.commit()
+
+        except:
+            session.rollback()
+            raise
+
+        finally:
+            session.close()
+        print("Item saved to database")
+        return item
---
+++ crawl/spiders/sites_spider.py
@@ -0,0 +1,54 @@
+import scrapy
+import csv
+import bs4
+import re
+import pandas as pd
+import time
+import sqlite3
+from scrapy.utils.python import to_native_str
+import logging
+
+logging.getLogger('scrapy').setLevel(logging.WARNING)
+
+
+class SitesSpider(scrapy.Spider):
+    name = "heritage"
+    urls = []
+    # furls = []
+    eresults = {}
+    fresults = {}
+    estatus = {}
+    fstatus = {}
+    dstatus = {}
+
+    def start_requests(self):
+        df = pd.read_excel(
+            '/Users/scottsyms/code/HeritageCanada/data/Python Results.xlsx')
+        count = 0
+        for index, row in df.iterrows():
+            count += 1
+            # , "handle_httpstatus_all": True}
+            site = {"language": "en", "pairid": count,
+                    "url": row["EnglishURL"], "altURL": row["FrenchURL"], "OriginalURL": row["EnglishURL"], "playwright": True}
+            yield scrapy.Request(url=site["url"], callback=self.parse, meta=site)
+            # , "handle_httpstatus_all": True}
+            site = {"language": "fr", "pairid": count,
+                    "url": row["FrenchURL"], "altURL": row["EnglishURL"], "OriginalURL": row["FrenchURL"], "playwright": True}
+            yield scrapy.Request(url=site["url"], callback=self.parse, meta=site)
+
+    def parse(self, response):
+
+        # If the response is a redirect, assemble the new URL and add a request to the iterator stack
+        # if response.status in [301, 302]:
+        #     newurl = response.urljoin(
+        #         response.headers['Location'].decode("utf-8"))
+        #     yield scrapy.Request(url=newurl, callback=self.parse, meta=response.meta)
+
+        # if response.status == 200:
+        # Remove the HTML from the response body
+        text = bs4.BeautifulSoup(response.body, "lxml").text.strip()
+        text = [line for line in text.split('\n') if line.strip() != '']
+        text = '\n'.join(text)
+
+        # Yield selected reponse fields
+        yield {'status': response.status, 'url': response.meta["OriginalURL"], 'language': response.meta["language"], 'text': text, "altURL": response.meta["altURL"], "pairid": response.meta["pairid"]}
